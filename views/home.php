af32ddfc7abb6b37a5601f51551d8e6d4fdb4a66
Paulo-Sergio <paulofranca.contato@gmail.com>
2017-05-24 14:26:47 -0300

    iniciando o layout da estrutura do dashboard

--- views/home.php
+++ views/home.php
@@ -1,1 +1,38 @@
-HOME
+<div class="db-row row-one">
+    <div class="grid-1">
+        <div class="db-grid-area">
+            <div class="db-grid-area-count">99</div>
+            <div class="db-grid-area-legend">Legenda</div>
+        </div>
+    </div>
+
+    <div class="grid-1">
+        <div class="db-grid-area">
+            <div class="db-grid-area-count">99</div>
+            <div class="db-grid-area-legend">Legenda</div>
+        </div>
+    </div>
+
+    <div class="grid-1">
+        <div class="db-grid-area">
+            <div class="db-grid-area-count">99</div>
+            <div class="db-grid-area-legend">Legenda</div>
+        </div>
+    </div>
+</div>
+
+<div class="db-row row-two">
+    <div class="grid-2">
+        <div class="db-info">
+            <div class="db-info-title">Titulo</div>
+            <div class="db-info-body">...</div>
+        </div>
+    </div>
+    
+    <div class="grid-1">
+        <div class="db-info">
+            <div class="db-info-title">Titulo</div>
+            <div class="db-info-body">...</div>
+        </div>
+    </div>
+</div>
